d92531120604444855f9d2453b2f0b0475c91f67
BenouKat <ben.goze@gmail.com>
2015-04-28 12:49:10 +0200

    Squelette serveur ok
    Next : Main menu !

---
+++ Assets/Script/Managers/ServerManager.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using PlayerIOClient;
+
+
+public class ServerManager : MonoBehaviour {
+
+	public static ServerManager instance;
+		
+	void Awake () {
+		if(instance == null) instance = this;
+		DontDestroyOnLoad (this);
+	}
+
+	//Player IO
+	private PlayerIOClient.Client PIOclient;
+	private Connection PIOconnection;
+
+	public bool connected;
+	public bool connectedToRoom;
+
+	public bool debug;
+
+	void onConnectionSuccess(string user, Client client){
+		Debug.Log("Connected !");
+		connected = true;
+		PIOclient = client;
+
+		joinSoloRoom (user);
+
+	}
+
+	void onRoomConnectionSuccess(string typeRoom, Connection connection)
+	{
+		Debug.Log("Room Joined !");
+		connectedToRoom = true;
+		PIOconnection = connection;
+		PIOconnection.OnMessage += onMessage;
+		PIOconnection.OnDisconnect += onDisconnect; 
+	}
+
+	void onDisconnect(object sender, string error){
+		connected = false;
+		connectedToRoom = false;
+		Debug.LogWarning("Disconnected !");	
+	}
+	
+	void onMessage(object sender, PlayerIOClient.Message m) {
+		Debug.Log (m.ToString ());
+	}
+
+	public void connect(string user, string password){
+		
+		Debug.Log("Connection...");
+		connected = false;
+		
+		PlayerIOClient.PlayerIO.Authenticate("cublast-2gjvwklc0aitw2udmikgq", "public",
+		                                     new Dictionary<string, string> {
+			{"username", user },
+			{"password", password }
+		},null,
+		//Success
+		delegate(Client client) {
+			PIOclient = client;
+			onConnectionSuccess(user, client);
+		},
+		//Error !
+		delegate(PlayerIOError error) {
+			Debug.LogError(error.Message);
+		}
+		);
+		
+	}
+	
+	public void register(string user, string password){
+		
+		PlayerIOClient.PlayerIO.Authenticate("cublast-2gjvwklc0aitw2udmikgq", "public",
+		                                     new Dictionary<string, string> {
+			{"register", "true" },
+			{"username", user },
+			{"password", password }
+		},null,
+		//Success
+		delegate(Client client) {
+			onConnectionSuccess(user, client);
+		},
+		//Error !
+		delegate(PlayerIOError error) {
+			Debug.LogError(error.Message);
+		}
+		);
+	}
+
+	public void joinSoloRoom(string user)
+	{
+		if(debug) {
+			PIOclient.Multiplayer.DevelopmentServer = new ServerEndpoint("127.0.0.1",8184);
+		}
+
+		connectedToRoom = false;
+
+		PIOclient.Multiplayer.CreateJoinRoom(user, "CublastSolo", false, null, null,
+			delegate(Connection connection) {
+
+				onRoomConnectionSuccess("CublastSolo", connection);
+			},
+			delegate(PlayerIOError error) {
+				Debug.LogError("Error : " + error.ToString());
+			}
+			);
+	}
+
+	public void joinOrCreateMultiplayerRoom(string user, string roomID, string roomName, bool publicRoom)
+	{
+		if(debug) {
+			PIOclient.Multiplayer.DevelopmentServer = new ServerEndpoint("127.0.0.1",8184);
+		}
+
+		connectedToRoom = false;
+
+		PIOclient.Multiplayer.CreateJoinRoom(roomID, "CublastMulti", publicRoom,  
+		                                     new Dictionary<string, string> { {"name", roomName} },
+											 new Dictionary<string, string> { {"user", user} },
+		//Success
+		delegate(Connection connection) {
+
+			onRoomConnectionSuccess("CublastMulti", connection);
+		},
+		//Error
+		delegate(PlayerIOError error) {
+			Debug.LogError("Error : " + error.ToString());
+		}
+		);
+	}
+	
+	public void disconnect(){
+		if (PIOconnection.Connected) {
+			PIOconnection.Disconnect();
+		}
+	}
+	
+
+}
---
+++ Serverside Code/Game Code/SoloGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using PlayerIO.GameLibrary;
+
+namespace Cublast {
+
+	[RoomType("CublastSolo")]
+	public class SoloGameCode : Game<Player> {
+
+		// This method is called when an instance of your the game is created
+		public override void GameStarted() {
+			// anything you write to the Console will show up in the 
+			// output window of the development server
+			Console.WriteLine("Game is started: " + RoomId);
+
+
+		}
+
+		// This method is called when the last player leaves the room, and it's closed down.
+		public override void GameClosed() {
+			Console.WriteLine("RoomId: " + RoomId);
+		}
+
+		// This method is called whenever a player joins the game
+		public override void UserJoined(Player player) {
+			foreach(Player pl in Players) {
+				if(pl.ConnectUserId != player.ConnectUserId) {
+					pl.Send("PlayerJoined", player.ConnectUserId);
+					player.Send("PlayerJoined", pl.ConnectUserId);
+				}
+			}
+		}
+
+		// This method is called when a player leaves the game
+		public override void UserLeft(Player player) {
+			Broadcast("PlayerLeft", player.ConnectUserId);
+		}
+
+		// This method is called when a player sends a message into the server code
+		public override void GotMessage(Player player, Message message) {
+			switch(message.Type) {
+				case "Chat":
+					foreach(Player pl in Players) {
+						if(pl.ConnectUserId != player.ConnectUserId) {
+							pl.Send("Chat", player.ConnectUserId, message.GetString(0));
+						}
+					}
+					break;
+			}
+		}
+	}
+}
